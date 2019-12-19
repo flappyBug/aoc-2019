802fd0a748e5b551fc6d34a0b86957df66f84577
Hourann <tdkgn@outlook.com>
2019-12-19 15:37:37 +0800

    move Process to mod intcode

--- src/day2.rs
+++ src/day2.rs
@@ -1,64 +1,12 @@
-use std::convert::TryInto;
-
-pub struct Process {
-    code: Vec<u32>,
-    ip: usize,
-}
-
-impl Process {
-    pub fn folk(&self) -> Self {
-        Self {
-            code: self.code.clone(),
-            ip: self.ip,
-        }
-    }
-
-    pub fn read<T: TryInto<usize>>(&self, addr: T) -> u32 {
-        self.code[addr.try_into().ok().unwrap()]
-    }
-
-    pub fn indirect_read<T: TryInto<usize>>(&self, addr: T) -> u32 {
-        self.read(self.read(addr))
-    }
-
-    pub fn write<T: TryInto<usize>, V: TryInto<u32>>(&mut self, addr: T, value: V) {
-        self.code[addr.try_into().ok().unwrap()] = value.try_into().ok().unwrap()
-    }
-
-    pub fn is_finished(&self) -> bool {
-        self.read(self.ip) == 99
-    }
-
-    pub fn step(&mut self) {
-        let ip = self.ip;
-        let op1 = self.indirect_read(ip + 1);
-        let op2 = self.indirect_read(ip + 2);
-        let value = match self.code[ip] {
-            1 => op1 + op2,
-            2 => op1 * op2,
-            _ => unreachable!(),
-        };
-        self.ip += 4;
-        self.write(self.code[ip + 3], value);
-    }
-
-    pub fn execute(&mut self) {
-        while !self.is_finished() {
-            self.step()
-        }
-    }
-}
+use crate::intcode::Process;
 
 #[aoc_generator(day2)]
 fn get_input(data: &str) -> Process {
-    Process {
-        code: data.split(',').map(|n| n.parse::<u32>().unwrap()).collect(),
-        ip: 0,
-    }
+    Process::new(data)
 }
 
 #[aoc(day2, part1)]
-fn part1(process: &Process) -> u32 {
+fn part1(process: &Process) -> i32 {
     let mut process = process.folk();
     process.write(1, 12);
     process.write(2, 2);
